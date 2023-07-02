5be6ece335836a1bea15a0217d925de0d16e7e08
nomkitha <119540549+nomkitha@users.noreply.github.com>
2023-07-02 22:58:57 +0200

    added axios

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
 
-import './App.css';
 
-function App() {
+import React from "react";
+import Weather from "./Weather";
+import "./App.css";
+
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        
-        <p>
-          Edit <code>src/App.js</code> hello react
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <Weather defaultCity="New York" />
+
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://www.linkedin.com/in/mbaliyezwe-nomkitha-ndzaba-31a7331b8/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Mbaliyezwe Nzaba
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/imbaliyezwe/the-react-weather-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            open-sourced on GitHub
+          </a>{" "}
+          and{" "}
+          <a
+            href="https://tangerine-yeot-5ea4e9.netlify.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            hosted on Netlify
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
-
-export default App;
